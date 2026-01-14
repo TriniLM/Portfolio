a6c574693c76335092344bc2a2bab66ca41811b2
TriniLM <gregoritrinidad110299@gmail.com>
2026-01-14 11:16:57 -0400

    reparando imagenes parallax

--- src/component/parallaxBanner/ParallaxBanner.jsx
+++ src/component/parallaxBanner/ParallaxBanner.jsx
@@ -3,9 +3,24 @@ import './parallaxBanner.css';
 export default function ParallaxBanner() {
   return (
     <div className={'parallax-container'}>
-        <img src='/parallax-banner/banner-one.webp' alt="imagen parallax tres" className={'parallax-one'}/>
-        <img src='/parallax-banner/banner-two.webp' alt="imagen parallax tres" className={'parallax-two'}/>
-        <img src='/parallax-banner/banner-three.webp' alt="imagen parallax tres" className={'parallax-thre'}/>
+        <img
+  src={`${import.meta.env.BASE_URL}parallax-banner/banner-one.webp`}
+  alt="imagen parallax uno"
+  className="parallax-one"
+/>
+
+<img
+  src={`${import.meta.env.BASE_URL}parallax-banner/banner-two.webp`}
+  alt="imagen parallax dos"
+  className="parallax-two"
+/>
+
+<img
+  src={`${import.meta.env.BASE_URL}parallax-banner/banner-three.webp`}
+  alt="imagen parallax tres"
+  className="parallax-three"
+/>
+
     </div>
   )
 }
